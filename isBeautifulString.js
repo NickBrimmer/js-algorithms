505e84acb37144984b3613e28f0d407420914e03
nickBrimmer <nickbrimmer@gmail.com>
2021-04-27 11:50:55 -0700

    findEmailDomain

--- isBeautifulString.js
+++ isBeautifulString.js
@@ -1,35 +1,25 @@
-function isBeautifulString(string) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz".split('').sort((a, b) => a - b)
-    string = string.split('').sort((a, b) => a - b).join('')
-    const chars = string.split(/[a-z]*([a-z])\1([a-z])\2[a-z]*/)
-    console.log(chars);
-    const count = {}
-
-    for (let i = 0; i < string.length; i++) {
-
-        if (count[string[i]] === undefined) {
-            count[string[i]] = 1;
-        } else {
-            count[string[i]]++
-        }
+const isBeautifulString = (inputString) => {
+    const alphabet = [];
+    
+    for (let i = 'a'.charCodeAt(); i <= 'z'.charCodeAt(); i++) {
+      alphabet.push(String.fromCharCode(i));
     }
-
-    for (let i = 0; i < string.length; i++) {
-
-
-        if (count[string[i]] < count[string[i + 1]]) {
-            return false
-        } else {
-            return true
-        }
+    
+    const letters = Array(alphabet.length).fill(0);
+    
+    for (let letter of inputString) {
+      letters[alphabet.indexOf(letter)]++;
     }
-}
-
-console.log(isBeautifulString("aabbb"));
-
-
-var newstring = "A11B22C33D44E555";
-
-var result = newstring.split(/(?=\D)/);
-
-console.log(result);
+    
+    for (let i = 0; i < letters.length; i++) {
+      if (letters[i + 1] > letters[i]) { return false; }
+    }
+    
+    return true;
+  };
+  
+  console.log(
+    isBeautifulString('bbbaacdafe'),
+    isBeautifulString('fyudhrygiuhdfeis'),
+    isBeautifulString('bbbaa'),
+  );
