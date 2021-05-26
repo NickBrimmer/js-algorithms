fc493dd0c752c7b3ba0602af21ae292ab6fcbd21
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2021-05-26 03:58:50 -0700

    buildPalindrome

---
+++ buildPalindrome.js
@@ -0,0 +1,11 @@
+function buildPalindrome() {
+
+};
+
+console.log(buildPalindrome("abcdc"))
+console.log(buildPalindrome("ababab"))
+console.log(buildPalindrome("abba"))
+console.log(buildPalindrome("abaa"))
+console.log(buildPalindrome("aaaaaba"))
+console.log(buildPalindrome("abc"))
+console.log(buildPalindrome("kebab"))
